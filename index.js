5d7754b95d633ac4aae8463b91a1a5bf1698a456
Ruminder Singh <ruminder.inbox@gmail.com>
2023-05-27 14:33:58 -0700

    Adding mongoose

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('/portfolio', (req, res) => res.send('Portfolio Page Route'));
 
 app.get('/contact', (req, res) => res.send('Contact Page Route'));
 
+
+
+const mongoose = require("mongoose");
+const uri = "mongodb+srv://igniteAdmin:6Yx0hbSU3dqsdW8F@webproject.88plx.mongodb.net/temp"
+console.log(uri);
+mongoose.connect(uri);
+const connection = mongoose.connection;
+
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server running on ${port}, http://localhost:${port}`));
